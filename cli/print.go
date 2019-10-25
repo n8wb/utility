75441aaaf6bec373a6c438dc00707803b9692cc8
n8wb <vanish172@gmail.com>
2019-10-25 08:08:34 -0700

    add pretty print to the cli utils

---
+++ cli/print.go
@@ -0,0 +1,36 @@
+package cli
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/Whiteblock/go-prettyjson"
+	"os"
+)
+
+func prettypi(i interface{}) string {
+	_, noPretty := os.LookupEnv("NO_PRETTY")
+	if noPretty {
+		out, _ := json.Marshal(i)
+		return string(out)
+	}
+	out, _ := prettyjson.Marshal(i)
+	return string(out)
+}
+
+func Print(i interface{}) {
+	switch i.(type) {
+	case string:
+		_, noPretty := os.LookupEnv("NO_PRETTY")
+		if noPretty {
+			fmt.Println(i.(string))
+		} else {
+			fmt.Printf("\033[97m%s\033[0m\n", i.(string))
+		}
+	default:
+		fmt.Println(prettypi(i))
+	}
+}
+
+func Printf(format string, a ...interface{}) {
+	Print(fmt.Sprintf(format, a...))
+}
