8d7bb193e8598003c7ea74fcab8231de512380fb
n8wb <vanish172@gmail.com>
2019-11-19 07:48:05 -0800

    license fix

--- interfaces/internals.go
+++ interfaces/internals.go
@@ -1,13 +1,13 @@
 /*
 	Copyright 2019 whiteblock Inc.
-	This file is a part of the genesis.
+	This file is a part of the utility.
 
-	Genesis is free software: you can redistribute it and/or modify
+	Utility is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
 	the Free Software Foundation, either version 3 of the License, or
 	(at your option) any later version.
 
-	Genesis is distributed in the hope that it will be useful,
+	Utility is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 	GNU General Public License for more details.
