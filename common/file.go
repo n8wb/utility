30a6d5335ebccb8cd5f6738aa005010fdc1338eb
n8wb <vanish172@gmail.com>
2019-12-24 14:21:57 -0800

    file metadata

--- common/file.go
+++ common/file.go
@@ -1,5 +1,12 @@
 package common
 
+import (
+	"fmt"
+	"path/filepath"
+	"strings"
+	"time"
+)
+
 // Metadata holds a bag of data representing information
 // about an uploaded file
 type Metadata struct {
@@ -13,4 +20,19 @@ type Metadata struct {
 	SizeBytes    int64     `json:"sizeBytes"`
 	MD5          string    `json:"hash"`
 	CreatedAt    time.Time `json:"createdAt"`
+}
+
+// FilenameWithPath preserves any path filename that was uploaded
+// with the file in multipath upload, we set the path as the file upload
+// formfile, ie:
+// -F key/path/blah=@file.ext
+func (m *Metadata) FilenameWithPath() string {
+	ext := filepath.Ext(m.Filename)
+	pathname := strings.TrimSuffix(m.Filename, ext)
+
+	if m.Path == m.Filename || m.Path == "" || m.Path == pathname {
+		return m.Filename
+	}
+
+	return fmt.Sprintf("%s/%s", m.Path, m.Filename)
 }
