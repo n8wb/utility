82842ed66c40596a288fbf1c00629729d09794b5
n8wb <vanish172@gmail.com>
2020-01-10 14:25:52 -0800

    add a SQL friendly format

--- common/test.go
+++ common/test.go
@@ -18,7 +18,10 @@
 
 package common
 
-import "time"
+import (
+	"database/sql"
+	"time"
+)
 
 // Test represents a whiteblock test
 type Test struct {
@@ -29,4 +32,39 @@ type Test struct {
 	Status         string    `json:"status" db:"status"`
 	DestroyedAt    time.Time `json:"destroyedAt" db:"destroyed_at"`
 	Name           string    `json:"name" db:"name"`
+}
+
+// Test represents a whiteblock test
+type TestSQL struct {
+	ID             string         `json:"id" db:"id"`
+	DefinitionID   string         `json:"definitionID" db:"definition_id"`
+	OrganizationID string         `json:"organizationID" db:"organization_id"`
+	CreatedAt      sql.NullTime   `json:"createdAt" db:"created_at"`
+	Status         sql.NullString `json:"status" db:"status"`
+	DestroyedAt    sql.NullTime   `json:"destroyedAt" db:"destroyed_at"`
+	Name           sql.NullString `json:"name" db:"name"`
+}
+
+func (ts TestSQL) Test() Test {
+	out := Test{
+		ID:             ts.ID,
+		DefinitionID:   ts.DefinitionID,
+		OrganizationID: ts.OrganizationID,
+	}
+	if ts.CreatedAt.Valid {
+		out.CreatedAt = ts.CreatedAt.Time
+	}
+
+	if ts.Status.Valid {
+		out.Status = ts.Status.String
+	}
+
+	if ts.DestroyedAt.Valid {
+		out.DestroyedAt = ts.DestroyedAt.Time
+	}
+
+	if ts.Name.Valid {
+		out.Name = ts.Name.String
+	}
+	return out
 }
