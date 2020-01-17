d78e6f0dd169721666e2c9c864eae0aa4795b261
n8wb <vanish172@gmail.com>
2020-01-17 09:19:11 -0800

    test logging util

---
+++ utils/logging.go
@@ -0,0 +1,35 @@
+/**
+ * Copyright 2019 Whiteblock Inc. All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
+package utils
+
+import (
+	"io/ioutil"
+	"testing"
+
+	"github.com/sirupsen/logrus"
+)
+
+type logrusHook struct {
+	t *testing.T
+}
+
+func (h logrusHook) Levels() []logrus.Level {
+	return logrus.AllLevels
+}
+
+func (h logrusHook) Fire(entry *logrus.Entry) error {
+	h.t.Log(entry.String())
+	return nil
+}
+
+func NewTestingLogger(t *testing.T) *logrus.Logger {
+	out := logrus.New()
+	out.SetLevel(logrus.TraceLevel)
+	out.SetOutput(ioutil.Discard)
+	out.AddHook(logrusHook{t: t})
+	return out
+}
