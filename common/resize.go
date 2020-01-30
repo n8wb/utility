8f3d0d0aaee26027763276a0cd6243416642fbd9
n8wb <vanish172@gmail.com>
2020-01-30 10:06:21 -0800

    add resize structure

---
+++ common/resize.go
@@ -0,0 +1,12 @@
+/**
+ * Copyright 2019 Whiteblock Inc. All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
+package common
+
+type Resize struct {
+	Height uint `json:"height"`
+	Width  uint `json:"width"`
+}
